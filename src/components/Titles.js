665cb5945a0c14999820dfb9dd8dece82df03264
Rafael Santiago <santiagorafa55@hotmail.com>
2019-01-31 01:48:20 -0200

    First commit

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container_title">Weather SPA</h1>
+    <h3 className="title-container_subtitle">
+      Find out temperature, condition and more information about the weather in
+      your area!
+    </h3>
+  </div>
+);
+
+export default Titles;
